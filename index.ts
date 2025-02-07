6dad3e69f36cf858fdeca13e899f5ec330ea75c8
bretheskevin <bretheskevin@gmail.com>
2025-02-07 12:05:44 +0100

    Refacto patients logic to router

--- index.ts
+++ index.ts
@@ -1,7 +1,6 @@
 import express, { Request, Response } from "express";
 import dotenv from "dotenv";
-import { PatientsController } from "./controllers/patients.controller";
-import { sendJsonResponse } from "./helpers/send-json-response";
+import patientsRouter from "./routers/patients.router";
 
 dotenv.config();
 
@@ -14,78 +13,7 @@ app.get("/", (req: Request, res: Response) => {
   res.status(200).send("GET request received");
 });
 
-app.get("/patients", (req: Request, res: Response) => {
-  const response = PatientsController.getPatients();
-  sendJsonResponse(res, response.toJSON());
-});
-
-app.get("/patients/:id", (req: Request, res: Response) => {
-  const { id } = req.params;
-
-  if (!parseInt(id)) {
-    res.status(400).send("400 Bad Request");
-    return;
-  }
-
-  const response = PatientsController.getPatient(parseInt(id));
-
-  if (!response) {
-    res.status(404).send("404 Not Found");
-    return;
-  }
-
-  sendJsonResponse(res, response.toJSON());
-});
-
-app.post("/patients", (req: Request, res: Response) => {
-  const { fullname } = req.body;
-
-  if (!fullname) {
-    res.status(400).send("400 Bad Request");
-    return;
-  }
-
-  const response = PatientsController.createPatient(fullname);
-
-  sendJsonResponse(res, response.toJSON());
-});
-
-app.put("/patients/:id", (req: Request, res: Response) => {
-  const { id } = req.params;
-  const { fullname } = req.body;
-
-  if (!parseInt(id)) {
-    res.status(400).send("400 Bad Request");
-    return;
-  }
-
-  if (!fullname) {
-    res.status(400).send("400 Bad Request");
-    return;
-  }
-
-  const response = PatientsController.updatePatient(parseInt(id), fullname);
-
-  if (!response) {
-    res.status(404).send("404 Not Found");
-    return;
-  }
-
-  sendJsonResponse(res, response.toJSON());
-});
-
-app.delete("/patients/:id", (req: Request, res: Response) => {
-  const { id } = req.params;
-
-  if (!parseInt(id)) {
-    res.status(400).send("400 Bad Request");
-    return;
-  }
-
-  PatientsController.deletePatient(parseInt(id));
-  res.status(200).send("DELETE request received");
-});
-
+app.use("/patients", patientsRouter);
 
 app.use((req: Request, res: Response) => {
   res.status(404).send("404 Not Found");
---
+++ routers/patients.router.ts
@@ -0,0 +1,79 @@
+import { Router, Request, Response } from "express";
+import {PatientsController} from "../controllers/patients.controller";
+import {sendJsonResponse} from "../helpers/send-json-response";
+
+const router = Router();
+
+router.get("/", (req: Request, res: Response) => {
+  const response = PatientsController.getPatients();
+  sendJsonResponse(res, response.toJSON());
+});
+
+router.get("/:id", (req: Request, res: Response) => {
+  const { id } = req.params;
+
+  if (!parseInt(id)) {
+    res.status(400).send("400 Bad Request");
+    return;
+  }
+
+  const response = PatientsController.getPatient(parseInt(id));
+
+  if (!response) {
+    res.status(404).send("404 Not Found");
+    return;
+  }
+
+  sendJsonResponse(res, response.toJSON());
+});
+
+router.post("/", (req: Request, res: Response) => {
+  const { fullname } = req.body;
+
+  if (!fullname) {
+    res.status(400).send("400 Bad Request");
+    return;
+  }
+
+  const response = PatientsController.createPatient(fullname);
+
+  sendJsonResponse(res, response.toJSON());
+});
+
+router.put("/:id", (req: Request, res: Response) => {
+  const { id } = req.params;
+  const { fullname } = req.body;
+
+  if (!parseInt(id)) {
+    res.status(400).send("400 Bad Request");
+    return;
+  }
+
+  if (!fullname) {
+    res.status(400).send("400 Bad Request");
+    return;
+  }
+
+  const response = PatientsController.updatePatient(parseInt(id), fullname);
+
+  if (!response) {
+    res.status(404).send("404 Not Found");
+    return;
+  }
+
+  sendJsonResponse(res, response.toJSON());
+});
+
+router.delete("/:id", (req: Request, res: Response) => {
+  const { id } = req.params;
+
+  if (!parseInt(id)) {
+    res.status(400).send("400 Bad Request");
+    return;
+  }
+
+  PatientsController.deletePatient(parseInt(id));
+  res.status(200).send("DELETE request received");
+});
+
+export default router;
